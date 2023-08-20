1fa829fe977c9ffa9dd9a025e0f33134ffe6f9f7
Shivesh Bhawnani <shivbhaw@github.com>
2023-08-20 22:26:13 +0200

    initializing webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: require.resolve('Phaser'),
+        loader: 'expose-loader',
+        options: { exposes: { globalName: 'Phaser', override: true } }
+      }
+    ]
+  },
+  devServer: {
+    static: path.resolve(__dirname, './'),
+    host: 'localhost',
+    port: 8080,
+    open: false
+  },
+  resolve: {
+    extensions: ['.js']
+  }
+};
